a44a86574ca356056d314481bb07eecdfaad4000
sim <sim9609@naver.com>
2021-02-15 13:05:30 +0900

    ok

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '$zxjg0k*4!taj5&g7$0vn&!zmucmrsaa9-gsv#!&m3e0l(2j&6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywherer.com']
 
@@ -124,4 +124,5 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
